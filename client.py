059776d2bdb47e3c39f3495b1c660a61b43086e7
Arshavir Ter-Gabrielyan <ter-gabrielyan@inf.ethz.ch>
2017-10-02 14:13:53 +0200

    Add command support; add timeout.

--- client.py
+++ client.py
@@ -8,27 +8,44 @@ import requests
 import json
 
 backends = ["silicon", "carbon"]
+command = ["verify", "terminate"]
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-p", "--port",
-                    help="Specify the port lestened by ViperServer.",
-                    type=int,
-                    required=True)
+parser.add_argument(
+    "-c", "--command",
+    help="Specify the command for the server.",
+    choices=command,
+    default="verify")
 
-parser.add_argument("-f", "--file",
-                    help="File to verify. Must be a Viper program.",
-                    required=True)
+parser.add_argument(
+    "-p", "--port",
+    help="Specify the port lestened by ViperServer.",
+    type=int,
+    required=True)
 
-parser.add_argument("-v", "--verifier",
-                    help="Specify which verification backend to use.",
-                    choices=backends,
-                    required=True)
+parser.add_argument(
+    "-f", "--file",
+    help="File to verify. Must be a Viper program.",
+    required=True)
 
-parser.add_argument("-x", "--options",
-                    help="Pass an options string to the verifier.")
+parser.add_argument(
+    "-v", "--verifier",
+    help="Specify which verification backend to use.",
+    choices=backends,
+    required=True)
+
+parser.add_argument(
+    "-x", "--options",
+    default="",
+    help="Pass an options string to the verifier.")
 
 args = parser.parse_args()
+
+if args.command == "terminate":
+    r = requests.get("http://localhost:" + str(args.port) + "/exit")
+    print(r.text)
+    sys.exit(0)
 
 if not Path(args.file).is_file():
     print("File `" + args.file + "` does not exist.")
@@ -37,12 +54,14 @@ if not Path(args.file).is_file():
 headers = {'Content-Type': 'application/json'}
 req = {'arg': args.verifier + ' ' + args.options + ' ' + args.file}
 
-r = requests.post("http://localhost:50424/verify",
+r = requests.post("http://localhost:" + str(args.port) + "/verify",
                   data=json.dumps(req),
-                  headers=headers)
+                  headers=headers,
+                  timeout=5)
 
 print(r.text)
 
-r = requests.get("http://localhost:50424/verify/" + str(r.json()["id"]))
+r = requests.get("http://localhost:" + str(args.port) + "/verify/" + str(r.json()["id"]),
+                 timeout=500)
 
 print(r.text)
