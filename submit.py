966990f612c5ecf808316614e11705e59e2598bb
Arshavir Ter-Gabrielyan <ter-gabrielyan@inf.ethz.ch>
2019-04-12 00:31:17 +0200

    Tuned default settings: for Windows and for Carbon.

--- submit.py
+++ submit.py
@@ -6,7 +6,7 @@ import os.path
 from pathlib import Path
 import requests
 import json
-import time
+import platform
 
 backends = ["silicon", "carbon"]
 command = ["verify", "terminate"]
@@ -28,17 +28,18 @@ parser.add_argument(
 parser.add_argument(
     "-f", "--file",
     help="File to verify. Must be a Viper program.",
-    required=True)
+    default="__empty_viper_file__.vpr")
 
 parser.add_argument(
     "-v", "--verifier",
     help="Specify which verification backend to use.",
-    choices=backends,
-    required=True)
+    #choices=backends,
+    default="silicon")
 
 parser.add_argument(
     "-x", "--options",
-    default="",
+    default=( "--disableCaching  --z3Exe=/usr/local/Viper/z3/bin/z3" if not platform.system()=="Windows" else
+              "--disableCaching '--z3Exe=C:\\Program Files\\Viper\\z3\\bin\\z3.exe'"),
     help="Pass an options string to the verifier.")
 
 args = parser.parse_args()
@@ -52,6 +53,24 @@ if not Path(args.file).is_file():
     print("File `" + args.file + "` does not exist.")
     sys.exit(1)
 
+if parser.get_default("verifier") == args.verifier:
+    print("[viper_client] Using default verification backend (Silicon). Reason: option -v is not provided.")
+
+if parser.get_default("file") == args.file:
+    print("[viper_client] Testing ViperClient with an empty Viper file. Reason: no file is specified via option -f.")
+
+if parser.get_default("options") == args.options:
+    if args.verifier != "carbon":
+        print("[viper_client] Default backend options set to: \n   " + args.options +
+              "\n   (Override with -x)")
+    else:
+        default_carbon_options=( "  --boogieExe=/usr/local/Viper/boogie/Binaries/Boogie" if platform.system()=="Darwin" else
+                                 " '--boogieExe=C:\\Program Files\\Viper\\boogie\\Binaries\\Boogie.exe'")
+        print("[viper_client] Default backend options (for carbon) set to: \n" +
+              "[viper_client]   " + args.options + default_carbon_options + "\n" +
+              "[viper_client]   (Override with -x)")
+        args.options += default_carbon_options
+
 headers = {'Content-Type': 'application/json'}
 req = {'arg': args.verifier + ' ' + args.options + ' ' +
        '"' + os.path.abspath(args.file) + '"'}
@@ -62,11 +81,3 @@ r = requests.post("http://localhost:" + str(args.port) + "/verify",
                   timeout=5)
 
 print(r.text)
-
-#r = requests.get("http://localhost:" + str(args.port) + "/exit",
-#                 stream=True)
-
-#r = requests.get("http://localhost:" + str(args.port) + "/discard/" +
-#                 str(r.json()["id"]),
-#                 stream=True)
-
